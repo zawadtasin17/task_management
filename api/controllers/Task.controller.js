f7867e9c0b0428962c8d3a48005ba319def69d6f
zawadtasin17 <zawadtasin@gmail.com>
2025-03-16 01:03:55 +0600

    created all apis

--- api/controllers/Task.controller.js
+++ api/controllers/Task.controller.js
@@ -1,25 +1,104 @@
 export const createTask = async (req, res) => {
     try{
         const {title, description} = req.body;
-        const task = new TaskModel({
+        const newTask = new TaskModel({
             title,
             description
         });
-        const newTask = await task.save();
+        await newTask.save();
         res.status(201).json
         ({
+            status: true, 
             message: "Task created successfully",
-            task: newTask
         });
     } catch (error) {
-        if (error instanceof ZodError) {
-            const getError = getZodError(error.errors);
-            setError(getError);
-        }
-        showToast("error", error.message);
+        res.status(201).json
+        ({
+            status: true, 
+            message: "Failed to create task",
+        });
     }
 }
-export const getAllTask = async (req, res) => {}
-export const showTask = async (req, res) => {}
-export const updateTask = async (req, res) => {}
-export const deleteTask = async (req, res) => {}
+export const getAllTask = async (req, res) => {
+    try{
+        const taskData = await TaskModel.find().sort({createdAt: -1}).lean().exec();
+
+        res.status(201).json
+        ({
+            status: true, 
+            taskData
+
+        });
+    } catch (error) {
+        res.status(201).json
+        ({
+            status: true, 
+            message: "Failed to get task",
+            task: newTask
+        });
+    }
+}
+export const showTask = async (req, res) => {
+    try{
+        const {taskid} = req.params;
+        const taskData = await TaskModel.findbyId(taskid).lean().exec();
+
+        res.status(201).json
+        ({
+            status: true, 
+            taskData
+
+        });
+    } catch (error) {
+        res.status(201).json
+        ({
+            status: true, 
+            message: "Failed to show task",
+            task: newTask
+        });
+    }
+}
+export const updateTask = async (req, res) => {
+    try{
+        const {taskid} = req.params;
+        const {title, description, status} = req.body;
+
+        const taskData = await TaskModel.findbyIdAndUpdate(taskid, {title, description, status}, {new: true});
+
+        const newTask = new TaskModel({
+            title,
+            description
+        });
+        await task.save();
+        res.status(201).json
+        ({
+            status: true, 
+            message: "Task updated successfully",
+        });
+    } catch (error) {
+        res.status(201).json
+        ({
+            status: true, 
+            message: error.message
+        });
+    }
+}
+export const deleteTask = async (req, res) => {
+    try{
+        const {taskid} = req.params;
+
+        await TaskModel.findbyIdAndDelete(taskid);
+
+        res.status(201).json
+        ({
+            status: true, 
+            message: "Task deleted successfully",
+        });
+    } catch (error) {
+        res.status(201).json
+        ({
+            status: true, 
+            message: error.message
+        });
+    }
+}
